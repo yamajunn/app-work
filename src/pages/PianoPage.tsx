25eeb851f680961df0bc21555f2bea1d5167156d
yamajunn <amiton1115@gmail.com>
2023-07-08 11:04:13 +0900

    add page

---
+++ src/pages/PianoPage.tsx
@@ -0,0 +1,19 @@
+// MUIのコンポーネントをインポート、使用するものはここで定義
+import { Box, SxProps, Theme } from "@mui/material";
+
+// 使用するコンポートをインポート
+
+// サンプルページのメインの実装
+export const PianoPage = () => {
+    return (
+        <Box className="PianoPage" sx={sx}>
+        </Box>
+    );
+};
+
+// クラス名に対するスタイルを定義
+const sx: SxProps<Theme> = {
+    "&.PianoPage": {
+        width: "100%",
+    }
+};
